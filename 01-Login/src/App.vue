275b8b1278dec1b8ee3bb803fe320b174dc37142
Rafael Noguera <rafanog2155@gmail.com>
2018-05-07 16:29:52 -0400

    Update App.vue

--- 01-Login/src/App.vue
+++ 01-Login/src/App.vue
@@ -11,6 +11,7 @@
           </router-link>
 
           <button
+            id="qsLoginBtn"
             class="btn btn-primary btn-margin"
             v-if="!authenticated"
             @click="login()">
@@ -18,6 +19,7 @@
           </button>
 
           <button
+            id="qsLogoutBtn"
             class="btn btn-primary btn-margin"
             v-if="authenticated"
             @click="logout()">
