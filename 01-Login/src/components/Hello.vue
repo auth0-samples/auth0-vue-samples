23ce11ae3a2adc30cd4a3b31067cc87526bf1b10
benlizar <36707032+benlizar@users.noreply.github.com>
2018-10-03 12:54:57 -0400

    added hello file for test

---
+++ 01-Login/src/components/Hello.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      message: 'hello!'
+    }
+  },
+  created () {
+    this.message = 'bye!'
+  }
+}
+</script>
