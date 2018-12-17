0597041e676a8a48b3317a8e9a3feef66ea50ecf
Steve Hobbs <elkdanger@gmail.com>
2018-12-17 14:10:33 +0000

    Rebased onto master

--- 01-Login/src/auth/AuthService.js
+++ 01-Login/src/auth/AuthService.js
@@ -82,49 +82,6 @@ class AuthService extends EventEmitter {
     this.emit(loginEvent, { loggedIn: false });
   }
 
-<<<<<<< HEAD
-  setSession (authResult) {
-    this.accessToken = authResult.accessToken
-    this.idToken = authResult.idToken
-    this.expiresAt = authResult.expiresIn * 1000 + new Date().getTime()
-
-    this.authNotifier.emit('authChange', { authenticated: true })
-
-    localStorage.setItem('loggedIn', true)
-  }
-
-  renewSession () {
-    this.auth0.checkSession({}, (err, authResult) => {
-      if (authResult && authResult.accessToken && authResult.idToken) {
-        this.setSession(authResult)
-      } else if (err) {
-        this.logout()
-        console.log(err)
-        alert(`Could not get a new token (${err.error}: ${err.error_description}).`)
-      }
-    })
-  }
-
-  logout () {
-    // Clear access token and ID token from local storage
-    this.accessToken = null
-    this.idToken = null
-    this.expiresAt = null
-
-    this.userProfile = null
-    this.authNotifier.emit('authChange', false)
-
-    localStorage.removeItem('loggedIn')
-
-    // navigate to the home route
-    router.replace('home')
-  }
-
-  isAuthenticated () {
-    // Check whether the current time is past the
-    // access token's expiry time
-    return new Date().getTime() < this.expiresAt && localStorage.getItem('loggedIn') === 'true'
-=======
   handleAuthentication() {
     return new Promise((resolve, reject) => {
       webAuth.parseHash((err, authResult) => {
@@ -186,7 +143,6 @@ class AuthService extends EventEmitter {
         }
       });
     });
->>>>>>> Updated sample
   }
 }
 
