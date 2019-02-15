b73219ae5fc0a534314fbffd900b9acc7f630367
Steve Hobbs <elkdanger@gmail.com>
2019-02-15 11:35:11 +0000

    Removed references to .env files in build

--- 01-Login/exec.ps1
+++ 01-Login/exec.ps1
@@ -1,2 +1,2 @@
 docker build --rm -t auth0-vue-01-login .
-docker run -p 3000:3000 --env-file .env --pid=host auth0-vue-01-login
+docker run -p 3000:3000 --pid=host auth0-vue-01-login
--- 03-Calling-an-API/exec.ps1
+++ 03-Calling-an-API/exec.ps1
@@ -1,2 +1,2 @@
 docker build --rm -t auth0-vue-03-api .
-docker run -p 3000:3000 --env-file .env --pid=host auth0-vue-03-api
+docker run -p 3000:3000 --pid=host auth0-vue-03-api
