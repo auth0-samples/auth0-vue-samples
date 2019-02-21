968afd50e437b6429876e1d2ee043a5501e72a75
Steve Hobbs <elkdanger@gmail.com>
2019-02-21 15:57:09 +0000

    Provided relevant content for homepage, fixed feature links

--- 03-Calling-an-API/src/views/Home.vue
+++ 03-Calling-an-API/src/views/Home.vue
@@ -19,7 +19,7 @@
       <div class="row">
         <div class="col-md-5">
           <h6 class="mb-3">
-            <a href="#">
+            <a href="https://auth0.com/docs/connections">
               <font-awesome-icon icon="link"/>Configure other identity providers
             </a>
           </h6>
@@ -30,33 +30,33 @@
 
         <div class="col-md-5">
           <h6 class="mb-3">
-            <a href="#">
+            <a href="https://auth0.com/docs/multifactor-authentication">
               <font-awesome-icon icon="link"/>Enable Multifactor Authentication
             </a>
           </h6>
-          <p>Auth0 supports social providers as Facebook, Twitter, Instagram and 100+, Enterprise providers as Microsoft Office 365, Google Apps, Azure, and more. You can also use any OAuth2 Authorization Server.</p>
+          <p>Add an extra layer of security by enabling Multi-factor Authentication, requiring your users to provide more than one piece of identifying information. Push notifications, authenticator apps, SMS, and DUO Security are supported.</p>
         </div>
       </div>
 
       <div class="row">
         <div class="col-md-5">
           <h6 class="mb-3">
-            <a href="#">
+            <a href="https://auth0.com/docs/anomaly-detection">
               <font-awesome-icon icon="link"/>Anomaly Detection
             </a>
           </h6>
-          <p>Auth0 supports social providers as Facebook, Twitter, Instagram and 100+, Enterprise providers as Microsoft Office 365, Google Apps, Azure, and more. You can also use any OAuth2 Authorization Server.</p>
+          <p>Auth0 can detect anomalies and stop malicious attempts to access your application. Anomaly detection can alert you and your users of suspicious activity, as well as block further login attempts.</p>
         </div>
 
         <div class="col-md"></div>
 
         <div class="col-md-5">
           <h6 class="mb-3">
-            <a href="#">
+            <a href="https://auth0.com/docs/rules">
               <font-awesome-icon icon="link"/>Learn About Rules
             </a>
           </h6>
-          <p>Auth0 supports social providers as Facebook, Twitter, Instagram and 100+, Enterprise providers as Microsoft Office 365, Google Apps, Azure, and more. You can also use any OAuth2 Authorization Server.</p>
+          <p>Rules are JavaScript functions that execute when a user authenticates to your application. They run once the authentication process is complete, and you can use them to customize and extend Auth0's capabilities.</p>
         </div>
       </div>
     </div>
