e8cdf8caf3f5906c20a4f7fe5b813ca36e6f56ab
Steve Hobbs <elkdanger@gmail.com>
2019-05-10 11:34:50 +0100

    Renamed server.js to web-server.js
    This had implications when running `npm start`. As there is no start
    script defined, npm tries to run a default script, which is not
    appropriate in this scenario.
    
    The `server.js` file has been renamed to avoid confusion.

--- 01-Login/exec.sh
+++ 01-Login/exec.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --rm -t auth0-vue-01-login .
-docker run -p 3000:3000 --pid=host auth0-vue-01-login
+docker build -t auth0-vue-01-login .
+docker run -p 3000:3000 --init auth0-vue-01-login
--- 03-Calling-an-API/exec.sh
+++ 03-Calling-an-API/exec.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --rm -t auth0-vue-03-api .
-docker run -p 3000:3000 --pid=host auth0-vue-03-api
+docker build -t auth0-vue-03-api .
+docker run -p 3000:3000 --init auth0-vue-03-api
