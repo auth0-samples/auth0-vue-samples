0530318b25023e6fc8aeb151a8e5b1f062887393
Steve Hobbs <steve.hobbs@auth0.com>
2020-11-03 11:24:30 +0000

    [SDK-2089] Vue sample updates (#121)
    * Ensure config options can be passed to loginWithPopup
    
    * Run npm update
    
    * Run npm audit fix
    
    * Spread all options to createAuth0Client
    
    * Apply fixes from 01-Login and update dependencies
    
    * Run npm audit fix
    
    * Simplify the passing of options

--- 01-Login/src/auth/authWrapper.js
+++ 01-Login/src/auth/authWrapper.js
@@ -27,11 +27,11 @@ export const useAuth0 = ({
       };
     },
     methods: {
-      async loginWithPopup(o) {
+      async loginWithPopup(options, config) {
         this.popupOpen = true;
 
         try {
-          await this.auth0Client.loginWithPopup(o);
+          await this.auth0Client.loginWithPopup(options, config);
           this.user = await this.auth0Client.getUser();
           this.isAuthenticated = await this.auth0Client.isAuthenticated();
           this.error = null;
@@ -73,9 +73,8 @@ export const useAuth0 = ({
     },
     async created() {
       this.auth0Client = await createAuth0Client({
-        domain: options.domain,
+        ...options,
         client_id: options.clientId,
-        audience: options.audience,
         redirect_uri: redirectUri
       });
 
