19e2147ff2b3b656544bfcaebfa1bb324d476d88
Frederik Prijck <frederik.prijck@auth0.com>
2022-02-03 17:48:35 +0100

    Migrate sample application to TypeScript (#179)

--- 01-Login/src/router/index.ts
+++ 01-Login/src/router/index.ts
@@ -1,9 +1,10 @@
-import { createRouter as createVueRouter, createWebHashHistory } from "vue-router";
+import { createRouter as createVueRouter, createWebHashHistory, Router } from "vue-router";
 import Home from "../views/Home.vue";
 import Profile from "../views/Profile.vue";
 import { createAuthGuard } from "@auth0/auth0-vue";
+import { App } from 'vue';
 
-export function createRouter(app) {
+export function createRouter(app: App): Router {
   return createVueRouter({
     routes: [
       {
