c584a3cbd103962ddca27811eac2ee93023facb5
Frederik Prijck <frederik.prijck@gmail.com>
2022-05-11 10:49:14 +0200

    Update code to match quickstart changes (#211)

--- 01-Login/src/views/Profile.vue
+++ 01-Login/src/views/Profile.vue
@@ -26,9 +26,10 @@ import { useAuth0 } from '@auth0/auth0-vue';
 export default {
   name: "profile",
   setup() {
-    const { user } = useAuth0();
+    const auth0 = useAuth0();
+    
     return {
-      user,
+      user: auth0.user,
     }
   }
 };
