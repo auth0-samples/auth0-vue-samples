a2fe510a801ef0aceeaa83bfad43fdc27a4edfe1
Ewan Harris <ewan.harris@okta.com>
2022-11-03 12:32:16 +0000

    Move to official highlight.js vue plugin

--- 02-Calling-an-API/src/main.ts
+++ 02-Calling-an-API/src/main.ts
@@ -2,19 +2,23 @@ import App from "./App.vue";
 import { createApp } from "vue";
 import { createRouter } from "./router";
 import { createAuth0 } from "@auth0/auth0-vue";
-import hljs from "vue3-highlightjs";
-import "highlight.js/styles/github.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faLink, faUser, faPowerOff } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import authConfig from "../auth_config.json";
+import hljs from 'highlight.js/lib/core';
+import json from 'highlight.js/lib/languages/json';
+import hljsVuePlugin from "@highlightjs/vue-plugin";
+import "highlight.js/styles/github.css";
+
+hljs.registerLanguage('json', json);
 
 const app = createApp(App);
 
 library.add(faLink, faUser, faPowerOff);
 
 app
-  .use(hljs)
+  .use(hljsVuePlugin)
   .use(createRouter(app))
   .use(
     createAuth0({
