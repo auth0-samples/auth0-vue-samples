bb314af54c05147267663263f13f408f56454db7
Frederik Prijck <frederik.prijck@auth0.com>
2022-12-13 11:19:07 +0100

    Configure proxy when calling an API (#345)

--- 02-Calling-an-API/vite.config.js
+++ 02-Calling-an-API/vite.config.js
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3001',
+    }
+  },
   plugins: [vue()]
 })
